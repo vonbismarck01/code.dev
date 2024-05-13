6e783c4a89a18b6ba8903285d15af8d9c3939351
von Bismarck <bismarckroyalvkb@gmail.com>
2024-05-13 17:08:12 -0300

    variaveis

--- index.php
+++ index.php
@@ -1,49 +1,31 @@
 <!DOCTYPE html>
-<html lang="pt=br">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
-<body>
-
-<?php
-$nome= "ola mundo";
-echo"<h1>$nome<h1>";
-
-?>
-<p><h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non placeat perspiciatis tenetur aspernatur quia nostrum repellendus amet, sunt, quibusdam adipisci accusamus animi quisquam temporibus voluptas dolorum dignissimos consequuntur doloremque. Neque?<p>
-
-<?php
-$x=10;
-$z=12;
-$y=12;
-$resultado = $x+ $z + $y;
-echo $resultado;
+    <link rel="stylesheet" href="./css/">
 
 
 
-?>
-<br>
-<br><br><br>
-
-
-<?php
-
-function media($a, $b, $c){
-$mediaFinal = ($a + $b + $c) / 3;
-
-
-return $mediaFinal;
-}
-echo (media(4, 8, 10));
-
-
-
-
-?>
-<h1>bismarck</h1>
-<h2>mais um projeto</h2>
     
+</head>
+
+<body>
+<h1>titulo em HTML</h1>
+
+<?php
+
+$nome = "jefferson";
+$peso = 91.80;
+$valor= 230;
+$ehaluno= true;
+$aluno ="ricardo";
+
+
+echo $peso;
+
+?>
+
 </body>
 </html>

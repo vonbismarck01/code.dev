3347f8225dcc4f49031bfe411baa20be5d983e71
von Bismarck <bismarckroyalvkb@gmail.com>
2024-05-20 17:05:10 -0300

    atualizalao.commit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,55 +9,15 @@
     <link href="https://cdn.jsdelivr.net/npm/@mdi/font@7.4.47/css/materialdesignicons.min.css" rel="stylesheet">
 </head>
 <body>
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <form action="" method="post">
-                <h1>Faça o seu cadastro</h1>
-                <br>
-                <label for="exampleInputEmail1" class="form-label">Endereço de email <i class="mdi mdi-information-outline" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Tooltip on right"></i> </label>
-                <br>
-                <input type="email" name="campoEmail" class="form-control"  id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text"></div>
-                <div class="invalid-feedback">
-                    <php?
-                    if(isset($erroEmail)){
-                        if($erroEmail !== "nenhum")){
-                            echo $erroEmail;
-                        }
-                    }
-                    >
-                   </div>
-                <br>
-                <label for="exampleInputEmail1" class="form-label">Nome</label>
-                <input type="nome" name="campoNome" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <br>
-                <label for="exampleInputPassword1" class="form-label">Sobrenome</label>
-                <input type="text" name="campoSobrenome" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <br>
-                <label for="exampleInputPassword1" class="form-label"> Senha</label>
-                <input type="password" name="campoSenha" class="form-control" id="exampleInputPassword1">
-                <label for="exampleInputPassword1" class="form-label">Confirmar senha</label>
-                <input type="password" name="confirmarSenha" class="form-control" id="exampleInputPassword1">
-                <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Não sou um robo</label> <br>
-                <br>
-
-                <button type="submit"  class="btn btn-primary">enviar</button> <br>
-
-            </form>
-        </div>
-    </div>
-</div>
 
 <?php
+
 if($_SERVER['REQUEST_METHOD']=="POST") {
     $email= $_POST['campoEmail'];
     $name= $_POST['campoNome'];
     $senha= $_POST['campoSenha'];
     $sobrenome= $_POST['campoSobrenome'];
-    $confirmarsenha= $_POST['confirmarsenha'];
+    $confirmarsenha= $_POST['confirmarSenha'];
     
     if(empty ($email) ){
         $erroEmail= "Informe seu email";
@@ -66,7 +26,7 @@ if($_SERVER['REQUEST_METHOD']=="POST") {
             $erroEmail= "Digite um email valido";
         }
         else{
-            $erroEmail ="Nenhum";
+            $erroEmail= "Nenhum";
         }
     }
     if(empty ($name)){
@@ -97,21 +57,111 @@ if($_SERVER['REQUEST_METHOD']=="POST") {
             $erroRepitaSenha ="As senhas precisam ser iguais";
         }else{ 
             $ErroRepitaSenha= "Nenhum";
-      }      
-   if($erroEmail=="Nehum" && $erroNome=="nenhum" && $erroSobrenome=="Nenhum" && $erroSenha== "Nenhum" && $erroConfirmarSenha=="nenhum" ) {
-    header(' location: Onrigado.html');
-
-    }
-    
-}   
+        }      
+        if($erroEmail=="Nehum" && $erroNome=="nenhum" && $erroSobrenome=="Nenhum" && $erroSenha== "Nenhum" && $erroConfirmarSenha=="nenhum" ) {
+            header('location: Onrigado.html');
+            
+        }
+        
+    }   
 }
+       ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <form action="" method="post">
+                <h1>Faça o seu cadastro</h1>
+                <br>
+                <label for="exampleInputEmail1" class="form-label">Endereço de email <i class="mdi mdi-information-outline" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Informe seu email"></i> </label>
+                <br>
+                <input type="email" name="campoEmail" class="form-control is-invalid  <?php if(isset($erroEmail)){if($erroEmail != "Nenhum"){;echo $erroEmail;}}?> "id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div class="invalid-feedback">
+                    <br>
+                    <?php
+                      if(isset($erroEmail)){
+                        if($erroEmail != "Nenhum"){;
+                        echo $erroEmail ;
+                      }
+                      }
+                    ?>
+                   </div>
+                <br>
+                <label for="exampleInputEmail1" class="form-label">Nome<i class="mdi mdi-information-outline" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Digitre seu primeiro nome"></i> </label>
+                <input type="nome" name="campoNome" class="form-control  is-invalid  <?php if(isset($erroNome)){if($erroNome != "Nenhum"){;echo $erroNome;}}
+               ?>" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div class="invalid-feedback">
+               <br>
+                <?php
+                      if(isset($erroNome)){
+                        if($erroNome != "Nenhum"){;
+                        echo $erroNome;
+                      }
+                      }
+               ?>
+                 </div>
+                <br>
+                <label for="exampleInputPassword1" class="form-label">Sobrenome <i class="mdi mdi-information-outline" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Digite o seu segundo nome"></i> </label>
+                <input type="text" name="campoSobrenome" class="form-control  is-invalid  <?phpif(isset($erroSobrenome)){if($erroSobrenome != "Nenhum"){;echo $erroSobrenome; } }?>
+                 </div>" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div class="invalid-feedback">
+                Please choose a username.
+                <?php
+                      if(isset($erroSobrenome)){
+                        if($erroSobrenome != "Nenhum"){;
+                        echo $erroSobrenome;
+                      }
+                      }
+                      ?>
+                 </div>
+              
+                <br>
+                <label for="exampleInputPassword1" class="form-label"> Senha <i class="mdi mdi-information-outline" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Sua senha"></i>  </label>
+                <input type="password" name="campoSenha" class="form-control   <?php if(isset($erroSenha)){if($erroSenha != "Nenhum"){;echo $erroSenha; }}?>" id="exampleInputPassword1"> 
+                <div class="invalid-feedback">
+                  Please choose a username.
+                  <?php
+                      if(isset($erroSenha)){
+                        if($erroSenha != "Nenhum"){;
+                        echo $erroSenha;
+                      }
+                      }
+                      ?>
+                </div>
+                
+                <label for="exampleInputPassword1" class="form-label">Confirmar senha <i class="mdi mdi-information-outline" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Repita sua senha"></i> </label>
+                <input type="password" name="confirmarSenha" class="form-control is-invalid " id="exampleInputPassword1"> 
+                <div class="invalid-feedback">
+                  Please choose a username.
+                  <?php
+                      if(isset($errocConfirmarSenha)){
+                        if($erroConfirmarSenha != "Nenhum"){;
+                        echo $erroConfirmarSenha;
+                      }
+                      }
+                      ?>
+                   </div>
+                <br>
+                <div class="mb-3 form-check">
+                <br>
+                <input type="checkbox" class="form-check-input is-invalid" id="exampleCheck1">
+                <label class="form-check-label " for="exampleCheck1">Não sou um robo</label> <br>
+                <br>
 
-?>
+                <button type="submit"  class="btn btn-primary">Enviar &nbsp<i class="mdi mdi-send"></i> <br>
+
+            </form>
+        </div>
+    </div>
+</div>
+
+
 
 
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script>const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))</script>
 
 </body>
 </html>
